ea5ed4204247b68dc7aa28329ea1630d7406ccca
mohbaba <babamu09@gmail.com>
2024-03-23 16:16:06 +0100

    Refactored: Created DiaryAppException

--- src/controllers/DiaryController.java
+++ src/controllers/DiaryController.java
@@ -1,4 +1,21 @@
 package controllers;
 
-//public class DiaryController {
-//}
+import data.Repositories.DiaryRepository;
+import data.Repositories.DiaryRepositoryImplementation;
+import dtos.requests.RegisterRequest;
+import services.DiaryService;
+import services.DiaryServiceImplementation;
+import services.Exceptions.DiaryAppException;
+
+public class DiaryController {
+    private final DiaryService diaryService = new DiaryServiceImplementation(new DiaryRepositoryImplementation());
+
+    public String registerUser(RegisterRequest registerRequest){
+        try {
+            diaryService.registerUser(registerRequest);
+            return "Registration Successful!";
+        }catch (DiaryAppException e){
+            return e.getMessage();
+        }
+    }
+}
